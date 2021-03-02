f4c7560bd32bf4cdb1b57723e0c87027e0413b5b
andres Guzmán <you@example.com>
2021-03-01 20:50:47 -0500

    0.9.6

--- app/app.py
+++ app/app.py
@@ -30,7 +30,7 @@ agregar_rutinas = AgregarRutina()
 asignar_rutina = Asignar()
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "https://proyectofinalsena.herokuapp.com/app/static"
+app.config['UPLOAD_FOLDER'] = "../app/static"
 CORS(app)
 
 app.secret_key = 'esto-es-una-clave-muy-secreta'
